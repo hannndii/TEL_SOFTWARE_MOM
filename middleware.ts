dc0f878dc4af1183cd4c0d5cee726857b017f7bd
hannndii <shiirogami95@gmail.com>
2026-07-21 09:49:07 +0700

    Clean code: rename supabase-proxy.ts back to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import { updateSession } from './utils/supabase/supabase-proxy'
+import { updateSession } from './utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
   try {
